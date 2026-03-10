eb16076b31cae28d13d010012f58e484daea976e
subashretrod <subashretrod@gmail.com>
2026-03-10 15:55:56 +0530

    comment the query

--- track.php
+++ track.php
@@ -8,23 +8,23 @@ include './admin/include/dbcon.php';
 
 
 // Check for spam keywords in blog content
-$query = "SELECT * FROM blog 
-          WHERE content LIKE '%slot%' 
-          OR content LIKE '%toto%' 
-          OR content LIKE '%gacor%'";
+// $query = "SELECT * FROM blog 
+//           WHERE content LIKE '%slot%' 
+//           OR content LIKE '%toto%' 
+//           OR content LIKE '%gacor%'";
 
-$result = mysqli_query($conn, $query);
+// $result = mysqli_query($conn, $query);
 
-if(mysqli_num_rows($result) > 0){
-    echo "Spam content found:<br><br>";
+// if(mysqli_num_rows($result) > 0){
+//     echo "Spam content found:<br><br>";
     
-    while($row = mysqli_fetch_assoc($result)){
-        echo "Blog ID: ".$row['id']."<br>";
-        echo "Title: ".$row['title']."<br>";
-        echo "Content Preview: ".substr(strip_tags($row['content']),0,200)."<br>";
-        echo "<hr>";
-    }
-}else{
-    echo "No spam content found in blog table.";
-}
+//     while($row = mysqli_fetch_assoc($result)){
+//         echo "Blog ID: ".$row['id']."<br>";
+//         echo "Title: ".$row['title']."<br>";
+//         echo "Content Preview: ".substr(strip_tags($row['content']),0,200)."<br>";
+//         echo "<hr>";
+//     }
+// }else{
+//     echo "No spam content found in blog table.";
+// }
 ?>
